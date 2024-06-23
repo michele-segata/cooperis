1d906ee2e5d0fa10634a761ca108776249fc9a3e
Miazzo Luigi <luigi.miazzo.93@gmail.com>
2024-06-23 18:43:05 +0200

    fix(WithCuda): remove device synchronization call

--- src/cooperis/cuda/WithCuda.cu
+++ src/cooperis/cuda/WithCuda.cu
@@ -28,7 +28,6 @@ void gain_compute_phase(int max_threads_per_block, const cuda_matrix& k_du_sin_c
 {
     int num_blocks = (k_du_sin_cos.rows * k_du_sin_cos.cols + max_threads_per_block - 1) / max_threads_per_block;
     gain_compute_phase_kernel<<<num_blocks, max_threads_per_block>>>(k_du_sin_cos.data, k_du_sin_sin.data, n, m, alpha, PHI, phase.data_real, phase.data_img, phase.rows * phase.cols);
-    cuda_assert(cudaDeviceSynchronize(), __FILE__, __LINE__);
 }
 
 void gsl_matrix_to_cuda_matrix(cuda_matrix& cuda, const gsl_matrix* gsl)
